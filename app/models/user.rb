71bf2ce9bd66794aceeee7378f09d18f795bad5c
Nick Vladimiroff <nickvladimiroff@gmail.com>
2016-08-14 00:01:38 -0400

    Add uniqueness for usernames.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.username = username.downcase }
-    validates :username, presence: true, length: { maximum: 50 }
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
     validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814012328) do
+ActiveRecord::Schema.define(version: 20160814040020) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "body"
     t.text     "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text     "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
