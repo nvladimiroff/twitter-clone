173eb414770b888b9ca76b408314b4fe2b643a3b
Nick Vladimiroff <nickvladimiroff@gmail.com>
2016-08-13 23:44:26 -0400

    Initial commit.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,47 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+  # GET /tweets
+  # GET /tweets.json
+  def index
+    @tweets = Tweet.all
+  end
+
+  # GET /tweets/1
+  # GET /tweets/1.json
+  def show
+      set_tweet
+  end
+
+  # GET /tweets/new
+  def new
+    @tweet = Tweet.new
+  end
+
+  # POST /tweets
+  # POST /tweets.json
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tweet_params
+      params.require(:tweet).permit(:body, :author)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    before_save { self.username = username.downcase }
+    validates :username, presence: true, length: { maximum: 50 }
+    validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
+end
